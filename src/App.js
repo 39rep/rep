79ded58cbe40a38c5058538227c2fd00a6914c29
39rep <okome3317@gmail.com>
2023-06-07 11:55:56 +0900

    push to Github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Contents from "./components/Contents";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Contents />
-      <Footer />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/aboutme" element={<AboutMe />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile">
+          <div className="upper">
+            <img src="./logo500.png" alt="icon" width={"100"} height={"100"} />
+            <h2 className="my-name">39rep</h2>
+          </div>
+          <div className="lower">
+            <p className="describe">にじさんじ好きな大学生</p>
+            <Link to="/aboutme">
+              <FontAwesomeIcon icon={faArrowRight} />
+              about me
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
